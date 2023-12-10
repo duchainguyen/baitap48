66aa0b41ebda7563578576d8895cd1a583c4511e
Miu <duchainguyen134@gmail.com>
2023-12-11 02:23:35 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App.jsx";
 
+const domain = "dev-oxdf8dfzdysbq72j.us.auth0.com";
+const clientId = "eyw124jhAiIovjZpIElm5ufUxXEtVkwg";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
-      domain="dev-oxdf8dfzdysbq72j.us.auth0.com"
-      clientId="eyw124jhAiIovjZpIElm5ufUxXEtVkwg"
+      domain={domain}
+      clientId={clientId}
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
