054af75073ff77aea5098d461c5fe8493c7aff0a
Miu <duchainguyen134@gmail.com>
2023-12-11 09:57:11 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import LoginButton from "./component/LoginButton";
 import Profile from "./component/Profile";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./App.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
 
@@ -13,6 +15,7 @@ function App() {
       ) : (
         <Profile user={user} logout={logout} />
       )}
+      <ToastContainer />
       {/* <LoginButton /> */}
     </div>
   );
--- src/component/Profile.jsx
+++ src/component/Profile.jsx
@@ -1,9 +1,35 @@
 import { useAuth0 } from "@auth0/auth0-react";
-import React from "react";
-import JSONPretty from "react-json-pretty";
-
+import React, { useRef } from "react";
+// import JSONPretty from "react-json-pretty";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
 const Profile = () => {
   const { user, isAuthenticated, logout } = useAuth0();
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_jz36l68",
+        "template_gnljds3",
+        form.current,
+        "kYyX-TD0KhtJqvcq4"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+          toast.success("Email sent successfully!");
+        },
+        (error) => {
+          console.log(error.text);
+          toast.error("Error sending email. Please try again.");
+        }
+      );
+  };
+
   return (
     isAuthenticated && (
       <div className="profile">
@@ -11,22 +37,15 @@ const Profile = () => {
           <img src={user.picture} alt="avatar" />
           <h2>Xin Chào {user.name}!</h2>
         </div>
-        <form action="">
+        <form ref={form} action="" onSubmit={sendEmail}>
           <div className="form-group">
             <label htmlFor="email">Company Email</label>
-            <input
-              //   ref={emailRef}
-              type="text"
-              id="email"
-              name="email"
-              value={"example@email.com"}
-              //   defaultValue={user.email ?? "example@email.com"}
-            />
+            <input type="email" name="from_email" value={"example@email.com"} />
           </div>
           <div className="form-group">
             <label htmlFor="textarea">Tin nhắn</label>
             <textarea
-              name="content"
+              name="message"
               id="textarea"
               rows="10"
               cols="50"
